4bb64470d802c4a5d86b32b74949d73ad34ccf28
Ryan J. Salva <ryanjsalva@github.com>
2021-10-12 01:10:24 +0000

    delete epam.py

--- epam.py
+++
@@ -1,7 +0,0 @@
-# import the image libraries
-from PIL import Image, ImageDraw, ImageFont
-# convert image to text
-import pytesseract
-#  Google Cloud Translate package
-from google.cloud import translate_v2
-
