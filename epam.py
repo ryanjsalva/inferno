37acba9e6301a2340a5255f058510a339d6354f7
Ryan J. Salva <ryanjsalva@github.com>
2021-10-12 03:17:46 +0000

    Add epam.py

---
+++ epam.py
@@ -0,0 +1,7 @@
+# import the image libraries
+from PIL import Image, ImageDraw, ImageFont
+# convert image to text
+import pytesseract
+#  Google Cloud Translate package
+from google.cloud import translate_v2
+
